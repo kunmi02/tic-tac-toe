5adc130d222f3903fe4b8819f67e87b9722e8633
kunmi02 <mrkhunmie@gmail.com>
2020-11-29 00:54:12 +0100

    fixed linter issues

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,5 @@
+# rubocop:disable Metrics/BlockNesting, Metrics/ClassLength, Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 class GameLogic
-
   def initialize
     @board = [['', '', ''], ['', '', ''], ['', '', '']]
   end
@@ -7,53 +7,51 @@ class GameLogic
   def validate_input_position(input)
     case input
     when 1
-      empty = @board[0][0].empty?
+      @board[0][0].empty?
     when 2
-      empty = @board[0][1].empty?
+      @board[0][1].empty?
     when 3
-      empty = @board[0][2].empty?
+      @board[0][2].empty?
     when 4
-      empty = @board[1][0].empty?
+      @board[1][0].empty?
     when 5
-      empty = @board[1][1].empty?
+      @board[1][1].empty?
     when 6
-      empty = @board[1][2].empty?
+      @board[1][2].empty?
     when 7
-      empty = @board[2][0].empty?
+      @board[2][0].empty?
     when 8
-      empty = @board[2][1].empty?
+      @board[2][1].empty?
     else
-      empty = @board[2][2].empty?
+      @board[2][2].empty?
     end
-      empty
   end
 
   def insert_user_option_to_board(input, player_sign)
-
-  case input
-  when 1
-    @board[0][0] = player_sign
-  when 2
-    @board[0][1] = player_sign
-  when 3
-    @board[0][2] = player_sign
-  when 4
-    @board[1][0] = player_sign
-  when 5
-    @board[1][1] = player_sign
-  when 6
-    @board[1][2] = player_sign
-  when 7
-    @board[2][0] = player_sign
-  when 8
-    @board[2][1] = player_sign
-  else
-    @board[2][2] = player_sign
-  end
+    case input
+    when 1
+      @board[0][0] = player_sign
+    when 2
+      @board[0][1] = player_sign
+    when 3
+      @board[0][2] = player_sign
+    when 4
+      @board[1][0] = player_sign
+    when 5
+      @board[1][1] = player_sign
+    when 6
+      @board[1][2] = player_sign
+    when 7
+      @board[2][0] = player_sign
+    when 8
+      @board[2][1] = player_sign
+    else
+      @board[2][2] = player_sign
+    end
     @board
   end
 
-  def check_winner(player, player_symbol)
+  def check_winner(_player, _player_symbol)
     combinations = [
       [@board[0][0], @board[0][1], @board[0][2]],
       [@board[1][0], @board[1][1], @board[1][2]],
@@ -62,14 +60,13 @@ class GameLogic
       [@board[2][0], @board[1][1], @board[0][2]],
       [@board[0][0], @board[1][0], @board[2][0]],
       [@board[0][1], @board[1][1], @board[2][1]],
-      [@board[0][2], @board[1][2], @board[2][2]],
-      
+      [@board[0][2], @board[1][2], @board[2][2]]
+
     ]
     combinations.any? do |combo|
-      if combo.all?('x') || combo.all?('o')
-        return true
-      end
+      return true if combo.all?('x') || combo.all?('o')
     end
   end
-
 end
+
+# rubocop:enable Metrics/BlockNesting, Metrics/ClassLength, Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,8 @@
-# player 
+# player
 class Player
   attr_accessor :player_name, :symbol
 
-  def  initialize(player_name, symbol)
+  def initialize(player_name, symbol)
     @player_name = player_name
     @symbol = symbol
   end
