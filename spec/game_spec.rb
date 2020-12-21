30976bfd3b82d95de8baf4e7e2e01f86c3a7ce08
kunmi02 <mrkhunmie@gmail.com>
2020-12-21 19:04:31 +0100

    linter fix

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../lib/game'
 
 describe GameLogic do
@@ -5,7 +7,7 @@ describe GameLogic do
     gamelogic = GameLogic.new
 
     it 'insert user option into the board position 1' do
-      expect(gamelogic.insert_user_option_to_board(1, 'x')).to eql([["x", "", ""], ["", "", ""], ["", "", ""]])
+      expect(gamelogic.insert_user_option_to_board(1, 'x')).to eql([['x', '', ''], ['', '', ''], ['', '', '']])
     end
 
     it 'cant insert into position 1 again' do
@@ -13,15 +15,14 @@ describe GameLogic do
     end
 
     it 'insert user option into the board position 2' do
-      expect(gamelogic.insert_user_option_to_board(2, 'x')).to eql([["x", "x", ""], ["", "", ""], ["", "", ""]])
+      expect(gamelogic.insert_user_option_to_board(2, 'x')).to eql([['x', 'x', ''], ['', '', ''], ['', '', '']])
     end
 
     it 'insert user option into the board position 3' do
-      expect(gamelogic.insert_user_option_to_board(3, 'x')).to eql([["x", "x", "x"], ["", "", ""], ["", "", ""]])
+      expect(gamelogic.insert_user_option_to_board(3, 'x')).to eql([%w[x x x], ['', '', ''], ['', '', '']])
     end
     it 'check winner' do
       expect(gamelogic.check_winner).to eql(true)
     end
   end
 end
-
