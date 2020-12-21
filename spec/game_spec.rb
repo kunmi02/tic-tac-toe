aa67d1ff42f98376f969f15f20c74e786877f0ab
kunmi02 <mrkhunmie@gmail.com>
2020-12-21 18:58:19 +0100

    added game spec for the game class

---
+++ spec/game_spec.rb
@@ -0,0 +1,27 @@
+require_relative '../lib/game'
+
+describe GameLogic do
+  describe '#validate_input_postion' do
+    gamelogic = GameLogic.new
+
+    it 'insert user option into the board position 1' do
+      expect(gamelogic.insert_user_option_to_board(1, 'x')).to eql([["x", "", ""], ["", "", ""], ["", "", ""]])
+    end
+
+    it 'cant insert into position 1 again' do
+      expect(gamelogic.validate_input_position(1)).to eql(false)
+    end
+
+    it 'insert user option into the board position 2' do
+      expect(gamelogic.insert_user_option_to_board(2, 'x')).to eql([["x", "x", ""], ["", "", ""], ["", "", ""]])
+    end
+
+    it 'insert user option into the board position 3' do
+      expect(gamelogic.insert_user_option_to_board(3, 'x')).to eql([["x", "x", "x"], ["", "", ""], ["", "", ""]])
+    end
+    it 'check winner' do
+      expect(gamelogic.check_winner).to eql(true)
+    end
+  end
+end
+
